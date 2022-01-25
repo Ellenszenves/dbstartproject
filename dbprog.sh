fb6d045570a3f14927e2b4bc48d702b6b47d4738
Ellenszenves <fallouterdo@gmail.com>
2022-01-25 18:15:58 +0100

    Setup.sh jelenleg nem üzemel, de már csak pár lépés, és működni fog. Kér egy felhasználónevet, jelszavat, a programmal majd ezzel lehet belépni az adatbázisba.

--- dbprog.sh
+++ dbprog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IP_db=$(cut -d ":" -f 1 /home/$USER/.pgpass)
-db_name='shop'
-db_user='test'
+db_name=$(cut -d ":" -f 3 /home/$USER/.pgpass)
+db_user=$(cut -d ":" -f 4 /home/$USER/.pgpass)
 
 #kategória felsorolás
 list_category() {
@@ -10,7 +10,7 @@ list_category() {
     unset data
     ezlenne=""
     idvar=""
-    azez=$(psql -t -h $IP_db -p 15432 -U test -d shop -c "SELECT * FROM categories")
+    azez=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c "SELECT * FROM categories")
     #azez=$($execute -h $IP_db -U test -d shop -c "SELECT * FROM categories")
     while IFS='|' read -r id nev description
     do
@@ -44,10 +44,10 @@ list_products() {
 #akkor csak az adott kategóriába tartozó termékek jelennek meg.
     if [ -n "$1" ]
     then
-    azez=$(psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    azez=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c \
     "SELECT product_name, unit_price FROM products WHERE category_id = '$1'")
     else
-    azez=$(psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    azez=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c \
     "SELECT product_name, unit_price FROM products")
     fi
     while IFS='|' read -r id nev description
@@ -66,7 +66,7 @@ list_products() {
     if [ -n "$select" ]
     then
     echo $cutted
-    inform=$(psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    inform=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c \
     "SELECT product_name, unit_price FROM products WHERE product_name = '$cutted';")
     zenity --info \
     --text="$inform" --width=300 --height=150
@@ -83,7 +83,7 @@ del_product() {
     unset data
     ezlenne=""
     idvar=""
-    azez=$(psql -t -h $IP_db -p 15432 -U test -d shop -c "SELECT product_name, unit_price FROM products")
+    azez=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c "SELECT product_name, unit_price FROM products")
     while IFS='|' read -r id nev description
     do
         ezlenne+="$nev "
@@ -97,7 +97,7 @@ del_product() {
     select=$(zenity --list --title="Termékek" --column="Név" --column="Ár" "${data[@]}")
     if [ -n "$select" ]
     then
-    inform=$(psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    inform=$(psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c \
     "DELETE FROM products WHERE product_name='$select'";)
     zenity --info \
     --text="$select törölve!"
@@ -127,7 +127,7 @@ add_category() {
     then
     while IFS='|' read -r name description
     do
-    psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    psql -t -h $IP_db -p 15432 -U $db_user -d $db_name -c \
     "INSERT INTO categories (category_name, description) VALUES ('$name', '$description') ;"
     done <<< "$add_cat"
     zenity --info \
@@ -149,7 +149,7 @@ add_product() {
     then
     while IFS='|' read -r name cat price
     do
-    psql -t -h $IP_db -p 15432 -U test -d shop -c \
+    psql -t -h $IP_db -p 15432 -U $db_user -d $db_user -c \
     "INSERT INTO products (product_name, category_id, unit_price) VALUES ('$name', '$cat', '$price') ;"
     done <<< "$add"
     zenity --info \
@@ -165,68 +165,81 @@ add_product() {
 #A kapott IP bekerül a .pgpass fájlba aminek a megfelelő jogosultágokat megadjuk
 #utána környezeti változóba kerül, ezután már nem kér jelszavat sem a program, mivel az a fájlban van.
 remote_server() {
-IP_db=$(zenity --forms --title="Távoli adatbázis" \
---add-entry="IP cím")
-pg_is_there=$(ls -la /home/$USER | grep -o .pgpass )
-if [ "$pg_is_here" == ".pgpass" ]
-then
-echo $IP_db:15432:shop:test:test > /home/$USER/.pgpass
-sudo chmod 600 /home/$USER/.pgpass
-sudo chown $USER:$USER /home/$USER/.pgpass
-export PGPASSFILE='/home/'$USER'/.pgpass'
-listen
-else
-touch /home/$USER/.pgpass
-echo $IP_db:15432:shop:test:test > /home/$USER/.pgpass
-sudo chmod 600 /home/$USER/.pgpass
-sudo chown $USER:$USER /home/$USER/.pgpass
-export PGPASSFILE='/home/'$USER'/.pgpass'
-listen
-fi
+    IP_db=$(zenity --forms --title="Távoli adatbázis" \
+    --add-entry="IP cím" \
+    --add-entry="Felhasználónév" \
+    --add-entry="Jelszó")
+    if [ -n "$IP_db" ]
+    then
+    while IFS='|' read -r protocoll username passwo
+        do
+        pg_is_there=$(ls -la /home/$USER | grep -o .pgpass )
+            if [ "$pg_is_here" == ".pgpass" ]
+            then
+            echo $protocoll:15432:shop:$username:$passwo > /home/$USER/.pgpass
+            sudo chmod 600 /home/$USER/.pgpass
+            sudo chown $USER:$USER /home/$USER/.pgpass
+            export PGPASSFILE='/home/'$USER'/.pgpass'
+            zenity --info --text="Kérem indítsa újra a programot!"
+            listen
+            else
+            touch /home/$USER/.pgpass
+            echo $protocoll:15432:shop:$username:$passwo > /home/$USER/.pgpass
+            sudo chmod 600 /home/$USER/.pgpass
+            sudo chown $USER:$USER /home/$USER/.pgpass
+            export PGPASSFILE='/home/'$USER'/.pgpass'
+            zenity --info --text="Kérem indítsa újra a programot!"
+            listen
+            fi
+        done <<< "$IP_db"
+    else
+    zenity --info --text="Ne hagyja üresen a mezőket!"
+    remote_server
+    fi
 }
 
 #Főmenü
 listen() {
-ans=$(zenity --list --title "Menü" --radiolist --column "ID" --column="Funkció" \
-1 'Felhasználó létrehozása' \
-2 'Felhasználó törlése' \
-3 'Termékek felsorolása' \
-4 'Termék hozzáadása' \
-5 'Termék törlése' \
-6 'Kategória hozzáadása' \
-7 'Kategóriák felsorolása' \
-8 'Kapcsolódás távoli adatbázishoz' \
-9 'teszt' --width=500 --height=500)
-if [ "$ans" == "Kategóriák felsorolása" ]
-then
-list_category
-elif [ "$ans" == "Felhasználó létrehozása" ]
-then
-listen
-elif [ "$ans" == "Felhasználó törlése" ]
-then
-dbsetup
-elif [ "$ans" == "Termék hozzáadása" ]
-then
-add_product
-elif [ "$ans" == "Kategória hozzáadása" ]
-then
-add_category
-elif [ "$ans" == "Termék törlése" ]
-then
-del_product
-elif [ "$ans" == "Termékek felsorolása" ]
-then
-list_products
-elif [ "$ans" == "teszt" ]
-then
-help
-elif [ "$ans" == "Kapcsolódás távoli adatbázishoz" ]
-then
-listen
-else
-exit
-fi
+    ans=$(zenity --list --title "Menü" --radiolist --column "ID" --column="Funkció" \
+    1 'Felhasználó létrehozása' \
+    2 'Felhasználó törlése' \
+    3 'Termékek felsorolása' \
+    4 'Termék hozzáadása' \
+    5 'Termék törlése' \
+    6 'Kategória hozzáadása' \
+    7 'Kategóriák felsorolása' \
+    8 'Kapcsolódás távoli adatbázishoz' \
+    9 'teszt' --width=500 --height=500)
+    if [ "$ans" == "Kategóriák felsorolása" ]
+    then
+    list_category
+    elif [ "$ans" == "Felhasználó létrehozása" ]
+    then
+    listen
+    elif [ "$ans" == "Felhasználó törlése" ]
+    then
+    dbsetup
+    elif [ "$ans" == "Termék hozzáadása" ]
+    then
+    add_product
+    elif [ "$ans" == "Kategória hozzáadása" ]
+    then
+    add_category
+    elif [ "$ans" == "Termék törlése" ]
+    then
+    del_product
+    elif [ "$ans" == "Termékek felsorolása" ]
+    then
+    list_products
+    elif [ "$ans" == "teszt" ]
+    then
+    help
+    elif [ "$ans" == "Kapcsolódás távoli adatbázishoz" ]
+    then
+    remote_server
+    else
+    exit
+    fi
 }
 
 starter() {
--- setup.sh
+++ setup.sh
@@ -1,51 +1,90 @@
 #!/bin/bash
 #A telepítések után újra kell indítani a gépet!
+dockercompose() {
+    user_pass=$(zenity --forms --title="Felhasználó" \
+    --add-entry="Felhasználónév" \
+    --add-entry="Jelszó")
+    if [ -n "$user_pass" ]
+    then
+    while IFS='|' read -r felh jelsz
+        do
+        read -r -d '' dokk << END
+version: '3.3'
+ 
+services:
+  db:
+    image: postgres:11
+    volumes:
+      - type: volume
+        source: db_data
+        target: /var/lib/postgres/data
+      - type: bind
+        source: ./starter.sql
+        target: /docker-entrypoint-initdb.d/starter.sql
+    restart: always
+      - POSTGRES_USER=$felh
+      - POSTGRES_PASSWORD=$jelsz
+      - POSTGRES_DB=shop
+    ports:
+      - "15432:5432"
+volumes:
+  db_data:
+END
+        done <<< "$user_pass"
+        echo "$dokk" > docker-compose.yml
+        docker-compose up -d
+    else
+    zenity --info --text="Üres mező nem engedélyezett!"
+    dockercompose
+    fi
+}
+
 dockerinstall() {
-docact="$(docker --version | grep -o "Docker")"
-if [ "$docact" == "Docker" ]
-then
-zenity --info --text="Docker már telepítve!"
-else
-sudo apt-get update
-sudo apt-get install -y docker
-sudo apt-get install -y docker-compose
-sudo usermod -aG docker $USER
-zenity --info --text="A telepítés befejeződött, kérem indítsa újra a számítógépet!"
-fi
+    docact="$(docker --version | grep -o "Docker")"
+    if [ "$docact" == "Docker" ]
+        then
+        zenity --info --text="Docker már telepítve!"
+        else
+        sudo apt-get update
+        sudo apt-get install -y docker
+        sudo apt-get install -y docker-compose
+        sudo usermod -aG docker $USER
+        zenity --info --text="A telepítés befejeződött, kérem indítsa újra a számítógépet!"
+    fi
 }
 
 postgresql_install() {
-posact="$(docker ps | grep -o "postgres")"
-if [ "$posact" == "postgres" ]
-then
-zenity --info --text="PostgreSQL aktív"
-else
-zenity --question --text="PostgreSQL nem aktív! Telepítsek?" \
---ok-label="Igen" --cancel-label="Ne"
-sudo chmod 777 starter.sql
-docker-compose up -d
-fi
-db_act="$(docker exec dbstartproject_db_1 psql -t -U test -d shop -c "SELECT * FROM teszt")"
-echo "$db_act"
-if [ "$db_act" == " aaaaa" ]
-then
-zenity --info --text="Az adatbázis lekérdezhető!"
-else
-zenity --question --text="Az adatbázis nem elérhető! Telepítsek?" \
---ok-label="Igen" --cancel-label="Ne"
-sudo chmod 777 starter.sql
-docker-compose up -d
-fi
-psql_act="$(psql --version | grep -o "psql")"
-if [ "$psql_act" == "psql" ]
-then
-zenity --info --text="PSQL kliens telepítve!"
-else
-zenity --question --text="PSQL kliens nincs telepítve! Telepítsem?" \
---ok-label="Igen" --cancel-label="Ne"
-sudo apt-get update
-sudo apt-get install -y postgresql
-fi
+    posact="$(docker ps | grep -o "postgres")"
+    if [ "$posact" == "postgres" ]
+        then
+        zenity --info --text="PostgreSQL aktív"
+        else
+        zenity --question --text="PostgreSQL nem aktív! Telepítsek?" \
+        --ok-label="Igen" --cancel-label="Ne"
+        sudo chmod 777 starter.sql
+        docker-compose up -d
+    fi
+    db_act="$(docker exec dbstartproject_db_1 psql -t -U test -d shop -c "SELECT * FROM teszt")"
+    echo "$db_act"
+    if [ "$db_act" == " aaaaa" ]
+        then
+        zenity --info --text="Az adatbázis lekérdezhető!"
+        else
+        zenity --question --text="Az adatbázis nem elérhető! Telepítsek?" \
+        --ok-label="Igen" --cancel-label="Ne"
+        sudo chmod 777 starter.sql
+        docker-compose up -d
+    fi
+    psql_act="$(psql --version | grep -o "psql")"
+        if [ "$psql_act" == "psql" ]
+        then
+        zenity --info --text="PSQL kliens telepítve!"
+        else
+        zenity --question --text="PSQL kliens nincs telepítve! Telepítsem?" \
+        --ok-label="Igen" --cancel-label="Ne"
+        sudo apt-get update
+        sudo apt-get install -y postgresql
+    fi
 }
 
 setup() {
@@ -62,4 +101,4 @@ setup() {
     postgresql_install
     fi
 }
-setup
+dockercompose
